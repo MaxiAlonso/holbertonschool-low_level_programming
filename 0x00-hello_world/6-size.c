1a90af0168786628353e9d014b84806ce345e957
MaxiHBTN <4387@holbertonschool.com>
2022-02-03 10:24:29 -0800

    6 Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,12 @@ int main(void)
 	int intType;
 	long int longintType;
 	long long int longlongintType;
+	float floatType;
 
 	printf("Size of a char: %zu byte(s) \n", sizeof(charType));
-	printf("Size of a int: %zu byte(s) \n", sizeof(intType));
+	printf("Size of an int: %zu byte(s) \n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s) \n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s) \n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s) \n", sizeof(floatType));
 	return (0);
 }
