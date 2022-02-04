457ebdb14a1df695ae001d6da14f7f44d50204bb
MaxiHBTN <4387@holbertonschool.com>
2022-02-04 12:04:04 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9' ; a++)
+	{
+	for (b = a ; b <= '9' ; b++)
+	{
+	for (c = b ; c <= '9' ; c++)
+	{
+		if (a != b && b != c)
+		{
+		putchar(a);
+		putchar(b);
+		putchar(c);
+		if (a < '8')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
