5a055c3a0511396a36d285500cc739ee3caf1bab
MaxiHBTN <4387@holbertonschool.com>
2022-02-04 10:44:12 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		putchar(num);
+		if (num < '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
