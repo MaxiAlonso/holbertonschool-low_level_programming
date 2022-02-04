c19457e3351894d179ae407d1fe074319b0d5955
MaxiHBTN <4387@holbertonschool.com>
2022-02-04 08:14:33 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+	putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
