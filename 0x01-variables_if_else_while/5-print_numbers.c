ea8e21cebb3b610795ff948c67bd65bb637fbe91
MaxiHBTN <4387@holbertonschool.com>
2022-02-04 09:18:25 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
