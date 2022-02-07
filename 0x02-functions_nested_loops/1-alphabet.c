e6cb75bc841daf0fae743b20576fc0b613ee7247
MaxiHBTN <4387@holbertonschool.com>
2022-02-07 07:35:18 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Return:
+ **/
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
