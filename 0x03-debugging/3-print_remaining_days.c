a676ae3285814f54e7853b558f56f7100a35ebb6
MaxiHBTN <4387@holbertonschool.com>
2022-02-08 15:36:21 -0800

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,8 +13,8 @@
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-
-		if (month >= 2 && day == 60)
+	{
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -23,8 +23,8 @@ void print_remaining_days(int month, int day, int year)
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
-	{
-		if (month == 2 && day < 60)
+	{i
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
