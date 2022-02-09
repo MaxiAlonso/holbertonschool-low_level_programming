153caf0f94d510e864099cf741fa2496b4022614
MaxiHBTN <4387@holbertonschool.com>
2022-02-09 10:11:43 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry poiny
+ * Return:0
+ **/
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	for (a = 1; a <= 100 ; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+		printf("FizzBuzz ");
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
