dd0744adcbc7af42b2c2cba3c591751a8a588c98
MaxiHBTN <4387@holbertonschool.com>
2022-02-09 05:24:41 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - rints the numbers, from 0 to 9, followed by a new line.
+ **/
+
+void print_numbers(void)
+{
+	char i;
+
+	i = '0';
+
+	for (i = '0' ; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
