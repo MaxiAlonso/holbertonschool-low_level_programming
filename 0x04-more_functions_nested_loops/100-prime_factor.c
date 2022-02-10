b0f35e68c52af212bc5fd9eea9ba2380a7f49377
MaxiHBTN <4387@holbertonschool.com>
2022-02-10 11:42:49 -0800

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ **/
+
+int main(void)
+{
+	long int n, p;
+
+	n = 612852475143;
+	p = 1;
+
+	while (n != 1)
+	{
+		p++;
+		while (n % p == 0)
+		{
+			n = (n / p);
+		}
+	}
+	printf("%ld \n", p);
+	return (0);
+}
