824c808407a66821d7fa4c810746e97d2edf4d70
MaxiHBTN <4387@holbertonschool.com>
2022-02-11 10:15:55 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- *_strlen - eturns the length of a string.
- * @s: string
+ * _strlen - returns the length of a string.
+ * Return: returns the lenght of a string.
+ * @s: pointer to a char
  **/
 
 int _strlen(char *s)
 {
 	int a, len;
 
-	len = 0;
 	a = 0;
+	len = 0;
 
 	for (a = 0; a <= *s++; a++)
 	{
 		len++;
 	}
+
 	return (len);
 }
