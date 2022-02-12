2c44161c39342bb0860c277957de5e45d423a6da
MaxiHBTN <4387@holbertonschool.com>
2022-02-11 17:11:08 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: dest
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
