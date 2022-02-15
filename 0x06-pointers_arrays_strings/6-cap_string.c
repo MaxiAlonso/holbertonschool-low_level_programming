f11ab936a549cd24f0c889430f4bd2f8316bc9ab
MaxiHBTN <4387@holbertonschool.com>
2022-02-15 10:50:50 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string.
+ * @str: string
+ * Return: string with all words capitalized
+ **/
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+		|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' ||
+		s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+		s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
