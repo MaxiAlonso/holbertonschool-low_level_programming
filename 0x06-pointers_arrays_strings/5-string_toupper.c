ec7d4a6f277a912b1fd828a66c33a8e6647936d5
MaxiHBTN <4387@holbertonschool.com>
2022-02-15 09:41:03 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: string
+ * Return: string in uppercase
+ **/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
