96d7fcb47708ac091f30653652af37b522ebaba8
MaxiHBTN <4387@holbertonschool.com>
2022-02-15 17:21:11 -0800

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ * @s: string
+ **/
+
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			{
+				s[i] = s[i] + 13;
+			}
+			else
+			{
+				s[i] = s[i] - 13;
+			}
+		break;
+		}
+		i++;
+	}
+	return (s);
+}
