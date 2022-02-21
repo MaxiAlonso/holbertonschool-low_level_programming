0add20ec2696f48b10aa5ab4827327c554ac8214
MaxiHBTN <4387@holbertonschool.com>
2022-02-21 10:40:44 -0800

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,8 +4,8 @@
  * *_strpbrk - searches a string for any of a set of bytes.
  * @s:string to be scanned
  * @accept: string containing the characters to match.
- * Return: returns a pointer to the byte in s that
- * matches one of the bytes in accept
+ * Return: returns a pointer to the byte in s
+ * that matches one of the bytes in accept
  **/
 
 char *_strpbrk(char *s, char *accept)
