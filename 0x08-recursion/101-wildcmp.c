b1eac649ea23ad1212adaf99ed0c9ca49f11ec35
MaxiHBTN <4387@holbertonschool.com>
2022-02-23 18:44:31 -0800

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
+/**
+ *
+ *
+ *
+ *
+ **/
 
+int wildcmp(char *s1, char *s2)
+{
+	if (*(s1 + 1) == *(s2 + 1))
+	{
+		return (wildcmp (s1 + 1, s2 + 2));
+	}
+	if (*(s1 + 1) != *(s2 + 1))
+	{
+		return (0);
+	}
+	return (1);
+}
