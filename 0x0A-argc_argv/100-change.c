721d5bc1fbb115a4cb0eca2c4a8f28f43c7b2bdf
MaxiHBTN <4387@holbertonschool.com>
2022-02-25 08:28:16 -0800

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,11 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, change, rest;
+	int change = 0, cents = 0, c = 0;
 
-	cents = 0;
-	rest = 0;
-	
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -23,17 +20,34 @@ int main(int argc, char *argv[])
 	change = atoi(argv[1]);
 	if (change / 25)
 	{
-		cents1 = cents + change / 25;
-		rest1 = change - cents * 25;
+		cents = cents + change / 25;
+		change = change - cents * 25;
+		c = cents;
 	}
-	if (rest1 / 10)
-        {
-                cents2 = cents + rest1 / 10;
-                rest2 = change - rest1 - (cents2 - cents1 * 10)
-        }
-
-	printf("%d\n", cents2);
-	printf("%d\n", rest1);
-	printf("%d\n", rest1);
+	if (change /10)
+	{
+		cents = cents + change / 10;
+		change = change - (cents - c) * 10;
+		c = cents;
+	}
+	if (change / 5)
+	{
+		cents = cents + change / 5;
+		change = change - (cents - c) * 5;
+		c = cents;
+	}
+	if (change / 2)
+	{
+		cents = cents + change / 2;
+		change = change - (cents - c) * 2;
+		c = cents;
+	}
+	if (change / 1)
+	{
+		cents = cents + change / 1;
+		change = change - (cents - c) * 1;
+		c = cents;
+	}
+	printf("%d\n", cents);
 	return (0);
 }
