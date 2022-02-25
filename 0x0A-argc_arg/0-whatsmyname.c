1bbbe3a487e8b14b2725420f61314ad0880cbd24
MaxiHBTN <4387@holbertonschool.com>
2022-02-25 04:23:54 -0800

    Task 0

--- 0x0A-argc_arg/0-whatsmyname.c
+++ 0x0A-argc_arg/0-whatsmyname.c
@@ -3,12 +3,12 @@
 
 /**
  * main - entry point
- * @argc - number of argunments
- * @argv - arguments
+ * @argc: number of argunments
+ * @argv: arguments
  * Return: 0
  **/
 
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
