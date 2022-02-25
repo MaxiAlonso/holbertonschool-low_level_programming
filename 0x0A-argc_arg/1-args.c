53fe33080c256b6bf2348291781fee68b098e08d
MaxiHBTN <4387@holbertonschool.com>
2022-02-25 04:33:27 -0800

    Task 1

---
+++ 0x0A-argc_arg/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of argunments
+ * @argv: arguments
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
