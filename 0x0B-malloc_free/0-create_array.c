d6b89c89b897b1f409c9525127b84e26c8b109de
MaxiHBTN <4387@holbertonschool.com>
2022-03-02 05:24:11 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size != 0)
 	{
-		a = malloc(sizeof(a) * size);
+		a = malloc(sizeof(c) * size);
 			if (a != NULL)
 			{
 				for (i = 0; i < size; i++)
