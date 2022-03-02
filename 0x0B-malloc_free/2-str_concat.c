42b70a822a02f1c89a17757cded308145a9808d0
MaxiHBTN <4387@holbertonschool.com>
2022-03-02 08:57:05 -0800

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,14 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
+	if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
 	while (s1[i])
 	{
 		i++;
@@ -23,16 +31,6 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j])
 	{
 		j++;
-	}
-	if (s1 == NULL)
-	{
-		s1 = "";
-		i = 1;
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-		j = 1;
 	}
 	s = malloc(sizeof(char) * (i + j + 1));
 	if (s != NULL)
