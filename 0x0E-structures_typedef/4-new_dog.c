31a39f9b398c01e27c6251c50bb280de6413335c
MaxiHBTN <4387@holbertonschool.com>
2022-03-08 19:02:36 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,15 @@
 #include "dog.h"
 
 /**
+ ** _strcpy - copies the string pointed to by src
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: dest
+ **/
+
+char *_strcpy(char *dest, char *src);
+
+/**
  * *new_dog -  creates a new dog.
  * @name: name of the dog
  * @age: age of the dog
@@ -12,18 +21,49 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *newdog;
+	dog_t *new_dog;
 
-	newdog = malloc(sizeof(struct dog));
-
-	if (newdog == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
+	new_dog->name = malloc(sizeof(name));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = malloc(sizeof(owner));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	_strcpy(new_dog->owner, owner);
+	return (new_dog);
+}
 
-	newdog->name = name;
-	newdog->age = age;
-	newdog->owner = owner;
+/**
+ ** _strcpy - copies the string pointed to by src
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: dest
+ **/
 
-	return (newdog);
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
 }
