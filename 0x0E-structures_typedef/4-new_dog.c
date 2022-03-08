67042334237f8f9f45f372be2d91d2012824aab2
MaxiHBTN <4387@holbertonschool.com>
2022-03-08 19:04:18 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,7 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	_strcpy(new_dog->name, name);
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
 	new_dog->owner = malloc(sizeof(owner));
 	if (new_dog->owner == NULL)
@@ -43,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog->name);
 		return (NULL);
 	}
-	_strcpy(new_dog->owner, owner);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
 
