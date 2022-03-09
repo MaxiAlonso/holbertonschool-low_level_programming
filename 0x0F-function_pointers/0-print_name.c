85fdd7a7d4780c4b45b59e9a3fce8fbac8388fae
MaxiHBTN <4387@holbertonschool.com>
2022-03-09 15:04:08 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: name to print
+ * @f: pointer to a function
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
