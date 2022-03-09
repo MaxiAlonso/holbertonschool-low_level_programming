a6181c669d63dd545d1f2c905bf462fae6d0cc5a
MaxiHBTN <4387@holbertonschool.com>
2022-03-09 18:51:20 +0000

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,16 +14,17 @@ int main(int argc, char *argv[])
 	int num1, num2, res, opc;
 	char *op;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	opc = argv[2][0];
-	op = argv[2];
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	opc = argv[2][0];
+	op = argv[2];
+
 	if (opc != '+' && opc != '-' && opc != '*' && opc != '/' && opc != '%')
 	{
 		printf("Error\n");
