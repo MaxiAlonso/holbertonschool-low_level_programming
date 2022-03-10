1730a76536e1a221b858b2faf24737e54c6967b6
MaxiHBTN <4387@holbertonschool.com>
2022-03-10 15:27:18 +0000

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,17 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 	int num;
-	
-	if (n !=0)
+
+	if (n != 0)
 	{
 		va_start(ap, n);
 		for (i = 0; i < n - 1; i++)
 		{
 			num = va_arg(ap, int);
-			printf("%d ", num);
+			printf("%d", num);
 			if (separator != NULL)
 			{
-				printf("%s", separator);
+				printf("%s ", separator);
 			}
 		}
 		num = va_arg(ap, int);
