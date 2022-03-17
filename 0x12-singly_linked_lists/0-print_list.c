ff0c795e4b1b556b876c289b352865b8dfd3d9bc
MaxiHBTN <4387@holbertonschool.com>
2022-03-17 14:31:23 +0000

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to a node.
+ * Return: number of nodes.
+ **/
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+
+	{
+		while (h != NULL)
+		{
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%d] %s\n", h->len, h->str);
+			}
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
