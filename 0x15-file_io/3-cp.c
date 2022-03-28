f05357932ff99802f5d37ba142d109de685fee2e
MaxiHBTN <4387@holbertonschool.com>
2022-03-28 19:04:56 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int from = 0, to = 0, r = 0, w = 0, c = 0, len = 1024;
+	int from = 0, to = 0, r = 1024, w = 0, c = 0;
 	char str[1024];
 
 	if (argc != 3)
@@ -21,32 +21,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(from, str, len);
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	w = write(to, str, len);
-	if (w != -1)
+	while (r == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		r = read(from, str, 1024);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		w = write(to, str, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
 	}
 	c = close(from);
 	if (c == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
-	}
 	c = close(to);
 	if (c == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
-	}
 	return (0);
 }
