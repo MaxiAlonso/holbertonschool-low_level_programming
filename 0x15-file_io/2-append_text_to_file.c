961663002a07890d68523eb160dc096bf6c1bccd
MaxiHBTN <4387@holbertonschool.com>
2022-03-28 16:03:27 +0000

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @filename: file of the name wich the text is going to be appended
  * @text_content: text to be appended
  * Return: 1 on success and -1 on failure
- **/ 
+ **/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
