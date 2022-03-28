3f92995915ac08464eeaf2b2cc253e3aa23703e1
MaxiHBTN <4387@holbertonschool.com>
2022-03-28 16:02:21 +0000

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: file of the name wich the text is going to be appended
- * @text_content: text to be appended.
+ * @text_content: text to be appended
  * Return: 1 on success and -1 on failure
  **/ 
 
