264f745b7c0f3cca634dbb62ba68f67ef96b229b
MaxiHBTN <4387@holbertonschool.com>
2022-05-05 12:30:01 +0000

    Header

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint - double linked list
+ * @num : integer stored as data
+ * @next: pointer to the next node
+ * @prev: pointer to the previous node
+ **/
+
+typedef struct dlistint{
+	int n;
+	struct dlistint *next;
+	struct dlistint *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
