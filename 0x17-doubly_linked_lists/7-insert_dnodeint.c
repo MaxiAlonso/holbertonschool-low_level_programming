fafcf884ff302aea254d343493406ebe4a65adb6
MaxiHBTN <4387@holbertonschool.com>
2022-05-05 16:44:38 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,26 @@
 #include "lists.h"
 
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the head node of the linked list.
+ * Return: numbers of elements in the linked list
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+
+	return (elements);
+}
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: pointer to the head node of the linked list.
@@ -13,36 +34,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 	dlistint_t *new_node, *current_node, *prev_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	if (*h == NULL)
+	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
+		new_node = add_dnodeint(h, n);
+		return (new_node);
 	}
-	else
+	if (idx == dlistint_len(*h))
 	{
+		new_node = add_dnodeint_end(h, n);
+		return (new_node);
+	}
+	if (*h != NULL)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
 		current_node = *h;
 		while (current_node != NULL)
 		{
 			prev_node = current_node->prev;
 			if (index == idx)
 			{
-				if (idx == 0)
-				{
-					new_node->prev = NULL;
-					new_node->next = current_node;
-					*h = new_node;
-				}
-				else
-				{
-					prev_node->next = new_node;
-					new_node->next = current_node;
-					new_node->prev = current_node->prev;
-				}
+				prev_node->next = new_node;
+				new_node->next = current_node;
+				new_node->prev = current_node->prev;
 			}
 			index++;
 			current_node = current_node->next;
