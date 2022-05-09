27c8478196c602673fc1457f443175104a9926cd
MaxiHBTN <4387@holbertonschool.com>
2022-05-09 19:18:17 +0000

    Task 3

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "opmain.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the addition of the two numbers
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract b from a
+ * @a: first number
+ * @b: second number
+ * Return: the substraction of b from a
+ **/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the multiplication of the two numbers
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a between b
+ * @a: first number
+ * @b: second number
+ * Return: the division of a between b
+ **/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - a modlue b
+ * @a: first number
+ * @b: second number
+ * Return: the remainder after dividing a by b.
+ **/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
