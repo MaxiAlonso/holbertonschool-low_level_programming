ddacc42513c11e6df4325fa0d9d5904a02599638
MaxiHBTN <4387@holbertonschool.com>
2022-05-27 15:10:43 +0000

    0x1A. C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(traverse->key, key) == 0)
 			{
+				free(traverse->value);
 				traverse->value = value_cpy;
 				return (1);
 			}
