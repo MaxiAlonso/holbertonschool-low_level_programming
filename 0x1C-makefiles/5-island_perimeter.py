64961be065818d400b43fd80d6ebec73d267ffc6
MaxiHBTN <4387@holbertonschool.com>
2022-07-14 17:37:05 +0000

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    row = len(grid)
+    column = len(grid[0])
+
+    perimeter = 0
+
+    for r in range(row):
+        for c in range(column):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if c > 0 and grid[r][c - 1] == 1:
+                   perimeter -= 2
+                if row > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+    return perimeter
