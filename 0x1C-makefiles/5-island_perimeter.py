192e55bc00ec105abde887af41a5c0c80e05c306
MaxiHBTN <4387@holbertonschool.com>
2022-07-14 17:39:11 +0000

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 """
 Island Perimeter
 """
+
 
 def island_perimeter(grid):
     """
@@ -18,7 +18,7 @@ def island_perimeter(grid):
             if grid[r][c] == 1:
                 perimeter += 4
                 if c > 0 and grid[r][c - 1] == 1:
-                   perimeter -= 2
+                    perimeter -= 2
                 if row > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
     return perimeter
