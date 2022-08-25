dd0144cd479699933a4884af4ffda6a1101e04b5
MaxiHBTN <4387@holbertonschool.com>
2022-08-25 18:58:01 +0000

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SAERCH_ALGOS_H
+#define SAERCH_ALGOS_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
