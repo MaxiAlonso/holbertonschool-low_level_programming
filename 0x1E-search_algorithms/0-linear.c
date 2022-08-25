7d6128951a065672fb7ee857d82e9848a195c460
MaxiHBTN <4387@holbertonschool.com>
2022-08-25 18:57:12 +0000

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Function that searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located
+ * or -1 if value is not present in array or if array is NULL,
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (array != NULL)
+	{
+		for (index = 0; index < size; index++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+			if (array[index] == value)
+				return (index);
+		}
+	}
+	return (-1);
+}
